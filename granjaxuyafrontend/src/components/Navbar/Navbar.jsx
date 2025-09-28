0da232a1de18718612d22b9d4f143428b58f60dd
JoseMarold <jmmr114@gmail.com>
2025-09-27 22:55:42 -0600

    Error in Navbar on quienes-somos link

--- granjaxuyafrontend/src/components/Navbar/Navbar.jsx
+++ granjaxuyafrontend/src/components/Navbar/Navbar.jsx
@@ -26,7 +26,7 @@ export default function Navbar() {
 
         <ul className="nav-list right">
           <li><a className="nav-link" href="#contacto">Contáctanos</a></li>
-          <li><Link className="nav-link" to="/quienes">¿Quiénes Somos?</Link></li>
+          <li><Link className="nav-link" to="/quienes-somos">¿Quiénes Somos?</Link></li>
           <li>
             <button className="cart-btn" aria-label="Carrito">
               <img
