644d222824aa585fb7c5cb3618a5c526c7adf9b6
Walter Villatoro <wevillatorom@correo.url.edu.gt>
2025-10-20 10:05:21 -0600

    Contact fixes v2

--- granjaxuyafrontend/src/pages/Contact/Contact.jsx
+++ granjaxuyafrontend/src/pages/Contact/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
     <div className="contact-container">
       <h1 className="contact-title">Contacto</h1>
       <div className="contact-grid">
-        <ContactCard title="Whatsapp" icon={<FaWhatsapp />} text="+502 5200 4438" link="https://wa.me/50212345678" />
+        <ContactCard title="Whatsapp" icon={<FaWhatsapp />} text="+502 3044 0927" link="https://wa.me/50230440927" />
         <ContactCard title="Instagram" icon={<FaInstagram />} text="@GranjaXuya" link="https://instagram.com/GranjaXuya" />
         <ContactCard title="Facebook" icon={<FaFacebook />} text="Granja Xuya" link="https://facebook.com/GranjaXuya.2024"/>
         <ContactCard title="TikTok" icon={<FaTiktok />} text="Granja Xuya" link="https://www.tiktok.com/@granjaxuya" />
